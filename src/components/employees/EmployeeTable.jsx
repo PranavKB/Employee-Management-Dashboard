d701d5b8e48a7c4f4c9e81def2081a8babac068f
PranavKB <39949849+PranavKB@users.noreply.github.com>
2026-01-04 00:27:48 +0530

    Employee Table logic

--- src/components/employees/EmployeeTable.jsx
+++ src/components/employees/EmployeeTable.jsx
@@ -1,7 +1,26 @@
+import { Table } from "antd";
+import { useEmployees } from "../../context/EmployeeContext";
 
 const EmployeeTable = () => {
+    const { employees } = useEmployees();
+
+    const columns = [
+    { title: "Employee ID", dataIndex: "employeeId" },
+    { title: "Name", dataIndex: "fullName" },
+    { title: "Gender", dataIndex: "gender" },
+    { title: "DOB", dataIndex: "dob" },
+    { title: "State", dataIndex: "state" },
+    {
+      title: "Active", dataIndex: "isActive", 
+      render: (isActive) => (isActive ? "Yes" : "No")
+    },
+    {
+      title: "Actions",
+    }
+  ];
+
   return (
-    <div>EmployeeTable</div>
+    <Table rowKey="id" columns={columns} dataSource={employees} />
   )
 }
 
