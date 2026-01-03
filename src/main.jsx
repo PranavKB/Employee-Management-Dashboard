682a5dc34091bbed0711dce450ac18d5b1e4226a
PranavKB <39949849+PranavKB@users.noreply.github.com>
2026-01-03 22:37:51 +0530

    Auth context and mock authentication logic

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import App from './App';
 import './styles/theme.css';
 import './styles/global.css';
 import 'antd/dist/reset.css';
+import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 )
