919bf50d2d5cf29506ae72cbe8b31ea30cdd3823
PranavKB <39949849+PranavKB@users.noreply.github.com>
2026-01-03 22:30:04 +0530

    Login page using antd

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,48 @@
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { Card, Form, Input, Button, Typography } from "antd";
+import './Login.css';
+
+const { Title } = Typography;
+
+export const Login = () => {
+  return (
+    <div className="login-container">
+      <Card className="login-card">
+        <Title level={3} className="login-title">
+          Dashboard Login
+        </Title>
+
+        <Form layout="vertical" className="login-form" name="login">
+          <Form.Item
+            label={<span className="login-label">Username</span>}
+            name="username"
+            rules={[{ required: true, message: "Please enter username" }]}
+          >
+            <Input
+                  prefix={<UserOutlined />}
+                  placeholder="Enter username"
+                  className="login-input"
+              />
+          </Form.Item>
+
+          <Form.Item
+            label={<span className="login-label">Password</span>}
+            name="password"
+            rules={[{ required: true, message: "Please enter password" }]}
+          >
+            <Input.Password
+                  prefix={<LockOutlined />}
+                  placeholder="Enter password"
+                  className="login-input-password"
+              />
+          </Form.Item>
+
+          <Button type="primary" htmlType="submit" block className="login-button">
+            Login
+          </Button>
+        </Form>
+      </Card>
+    </div>
+  );
+}
+export default Login;
