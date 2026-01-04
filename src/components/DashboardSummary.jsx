fc192f5903f92de85d6c4f5f07184f39b96310c6
PranavKB <39949849+PranavKB@users.noreply.github.com>
2026-01-04 06:49:35 +0530

    Summary from fetched data

--- src/components/DashboardSummary.jsx
+++ src/components/DashboardSummary.jsx
@@ -1,22 +1,26 @@
 import { Card, Col, Row, Statistic } from "antd";
+import { useEmployees } from "../context/EmployeeContext";
 
 const DashboardSummary = () => {
+  const { employees } = useEmployees();
+  const activeCount = employees.filter(e => e.isActive).length;
+
   return (
     <>
       <Row gutter={16}>
         <Col span={8}>
           <Card size="small">
-            <Statistic title="Total Employees" value={100} />
+            <Statistic title="Total Employees" value={employees.length} />
           </Card>
         </Col>
         <Col span={8}>
           <Card size="small">
-            <Statistic title="Active Employees" value={85} />
+            <Statistic title="Active Employees" value={activeCount} />
           </Card>
         </Col>
         <Col span={8}>
           <Card size="small">
-            <Statistic title="Inactive Employees" value={15} />
+            <Statistic title="Inactive Employees" value={employees.length - activeCount} />
           </Card>
         </Col>
       </Row>
